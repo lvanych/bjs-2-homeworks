e97ebcc85abc2088f6cbaaad64781cf22a286475
Ivanych <lvanych@mail.ru>
2023-12-03 10:15:51 +0400

    Четвёртое задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.every((element) => arr1[element] === arr2[element]) && arr1.length === arr2.length;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(user => user.gender === gender).reduce((acc, user, index, array) => acc + user.age / array.length, 0)
+  return result
 }
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
+let student1 = new Student('Алексей', 'Мужской', 37);
+let student2 = new Student('Анжела', 'Женский', 25);
+let student3 = new Student('Святослав', 'Мужской', 42);
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+    if (this.marks) {
+      this.marks.push(...marks);
+    }
+  }
 
 Student.prototype.getAverage = function () {
-  
-}
+    if (!this.marks || this.marks.length === 0) {
+         return 0;
+    }
+    return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
+ }
 
-Student.prototype.exclude = function (reason) {
-  
-}
+ Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
